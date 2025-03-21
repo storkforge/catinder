6c38c7d2f6b50613f4ac0b3268d68fbb3c009a37
Malva Malmgren <malva.malmgren@gmail.com>
2025-03-21 13:23:44 +0100

    Initial commit

---
+++ src/test/java/org/example/springboot25/Springboot25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
