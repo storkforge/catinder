7d6b4214a1b063fe2536657e9c6d0669b23381c7
Eva Hottinen <ehottinen@gmail.com>
2025-03-26 21:31:49 +0100

    Typo

--- src/main/java/org/example/springboot25/controller/RecommendationController.java
+++ src/main/java/org/example/springboot25/controller/RecommendationController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/recommendations")
