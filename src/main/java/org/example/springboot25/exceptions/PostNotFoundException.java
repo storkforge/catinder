34658e81db620a32544ea3637988b345474a479b
Eva Hottinen <ehottinen@gmail.com>
2025-03-28 15:54:48 +0100

    Deleted postNotFoundException.java

--- src/main/java/org/example/springboot25/exceptions/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.springboot25.exceptions;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
