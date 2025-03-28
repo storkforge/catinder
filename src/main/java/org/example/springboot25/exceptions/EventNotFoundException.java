8da4b802268b914a75bf5f108b78cad053521940
Eva Hottinen <ehottinen@gmail.com>
2025-03-28 15:44:08 +0100

    Deleted EventNotFoundException.java

--- src/main/java/org/example/springboot25/exceptions/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.springboot25.exceptions;
-
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(String message) {
-        super(message);
-    }
-}
