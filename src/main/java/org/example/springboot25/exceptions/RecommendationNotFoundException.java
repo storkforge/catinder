04adab0cc32e4e5c23dab7b3d87da568fc0228f2
Eva Hottinen <ehottinen@gmail.com>
2025-03-28 15:57:00 +0100

    Deleted RecommendationNotFoundException

--- src/main/java/org/example/springboot25/exceptions/RecommendationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.springboot25.exceptions;
-
-public class RecommendationNotFoundException extends RuntimeException {
-    public RecommendationNotFoundException(String message) {
-        super(message);
-    }
-}
