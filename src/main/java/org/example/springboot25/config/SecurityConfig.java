9547f4420a5899188064f6ca9850ab4df4f0828a
Malva Malmgren <malva.malmgren@gmail.com>
2025-04-03 18:21:33 +0200

    Add Spring Security dependency and config package

---
+++ src/main/java/org/example/springboot25/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package org.example.springboot25.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+}
