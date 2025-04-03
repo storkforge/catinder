e2b8bb2ea35f3e324bac78ece0ae39340982e04a
Eva Hottinen <ehottinen@gmail.com>
2025-04-03 08:49:46 +0200

    Renamed GlobalExceptionHandler

--- src/main/java/org/example/springboot25/exceptions/GlobalRESTExceptionHandler.java
+++ src/main/java/org/example/springboot25/exceptions/GlobalRESTExceptionHandler.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalRESTExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(org.example.springboot25.exceptions.GlobalExceptionHandler.class);
 
