91f414a5d79d989c3d1e9d4c4ac79d92ec59ee78
Eva Hottinen <ehottinen@gmail.com>
2025-04-04 10:20:34 +0200

    Security commit so I don't mess anything up when I checkout

--- src/main/java/org/example/springboot25/security/CustomUserDetailsService.java
+++ src/main/java/org/example/springboot25/security/CustomUserDetailsService.java
@@ -1,4 +1,29 @@
 package org.example.springboot25.security;
 
-public class CustomUserDetailsService {
+import org.example.springboot25.entities.User;
+import org.example.springboot25.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // ladda user genom användarnamn/email
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return new CustomUserDetails(user);
+    }
 }
