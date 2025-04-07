9b3c5d2b4fd82db118363d159b1be49034fccffe
Eva Hottinen <ehottinen@gmail.com>
2025-04-07 08:51:04 +0200

    Use constructor-based dependency injection for PostService and userService

--- src/main/java/org/example/springboot25/controller/AdminController.java
+++ src/main/java/org/example/springboot25/controller/AdminController.java
@@ -15,6 +15,11 @@ public class AdminController {
     private final PostService postService;
     private final UserService userService;
 
+    public AdminController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
     // Visa alla inlägg (för moderering)
     @GetMapping("/posts")
     public String showAllPosts(Model model) {
