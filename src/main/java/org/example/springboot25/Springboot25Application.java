9153274522ec020f4189ec532af4ef0b378c813d
Minhya <minhy96th@gmail.com>
2025-04-08 14:56:36 +0200

    Added Locale ENGLISH in Application

--- src/main/java/org/example/springboot25/Springboot25Application.java
+++ src/main/java/org/example/springboot25/Springboot25Application.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import java.util.Locale;
+
 @SpringBootApplication
 @EnableWebSecurity
 public class Springboot25Application {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(Springboot25Application.class, args);
     }
 
