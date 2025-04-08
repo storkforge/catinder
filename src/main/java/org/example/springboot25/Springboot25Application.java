1bad39e95fc97444f647cf7e00f4abe8befbc7b4
Minhya <minhy96th@gmail.com>
2025-04-08 14:56:36 +0200

    Added Locale ENGLISH in Application

--- src/main/java/org/example/springboot25/Springboot25Application.java
+++ src/main/java/org/example/springboot25/Springboot25Application.java
@@ -3,10 +3,13 @@ package org.example.springboot25;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class Springboot25Application {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(Springboot25Application.class, args);
     }
 
