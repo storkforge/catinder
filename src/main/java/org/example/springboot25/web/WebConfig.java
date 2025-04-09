e6579b378483774a718b22e836a645368f622084
Minhya <minhy96th@gmail.com>
2025-04-10 01:54:17 +0200

    Removed unused properties and added space to between attributes

--- src/main/java/org/example/springboot25/web/WebConfig.java
+++ src/main/java/org/example/springboot25/web/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
