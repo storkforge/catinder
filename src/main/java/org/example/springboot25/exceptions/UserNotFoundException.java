d3f7e75fbe1dc2041256be3ce2471633cc9c7a6d
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-10 22:00:33 +0200

    Add custom UserNotFoundException

---
+++ src/main/java/org/example/springboot25/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.springboot25.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
