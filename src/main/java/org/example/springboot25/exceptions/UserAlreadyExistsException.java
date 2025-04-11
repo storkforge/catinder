bd87e802d4bf25c5247a0426f102f19457c66829
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-11 11:13:46 +0200

    Rename UserAlreadyExistsException to AlreadyExistsException for consistency

--- src/main/java/org/example/springboot25/exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.springboot25.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
