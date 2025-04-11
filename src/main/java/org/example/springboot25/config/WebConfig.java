69971b6396ef8f2afda2442e71dc5b4d69c43480
Minhya <minhy96th@gmail.com>
2025-04-11 14:20:03 +0200

    Moved webconfig to config and removed web folder

--- src/main/java/org/example/springboot25/config/WebConfig.java
+++ src/main/java/org/example/springboot25/config/WebConfig.java
@@ -1,4 +1,4 @@
-package org.example.springboot25.web;
+package org.example.springboot25.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
