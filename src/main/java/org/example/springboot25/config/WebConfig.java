ccbd3dcd94f4c14bb1f9cc840c0f0b03025af8ff
Minhya <minhy96th@gmail.com>
2025-04-11 16:03:25 +0200

    Fixed coderabbits nitpicks

--- src/main/java/org/example/springboot25/config/WebConfig.java
+++ src/main/java/org/example/springboot25/config/WebConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -27,6 +29,17 @@ public class WebConfig implements WebMvcConfigurer {
         return localeResolver;
 
     }
+    @Bean
+public LocaleChangeInterceptor localeChangeInterceptor() {
+            LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+            interceptor.setParamName("lang");
+            return interceptor;
+        }
+
+        @Override
+public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(localeChangeInterceptor());
+        }
 
 
 }
