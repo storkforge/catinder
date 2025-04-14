abe648739e278a3d25e4e0ce7b284e12af460bd5
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-14 17:22:42 +0200

    Enhance EventParticipantUpdateDTO with validation, constructors, and utility methods

--- src/main/java/org/example/springboot25/dto/EventParticipantUpdateDTO.java
+++ src/main/java/org/example/springboot25/dto/EventParticipantUpdateDTO.java
@@ -4,6 +4,14 @@ public class EventParticipantUpdateDTO {
     private String userName;
     private String eventName;
 
+    public EventParticipantUpdateDTO() {
+    }
+
+    public EventParticipantUpdateDTO(String userName, String eventName) {
+        this.userName = userName;
+        this.eventName = eventName;
+    }
+
     public String getUserName() {
         return userName;
     }
@@ -19,4 +27,30 @@ public class EventParticipantUpdateDTO {
     public void setEventName(String eventName) {
         this.eventName = eventName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventParticipantUpdateDTO that = (EventParticipantUpdateDTO) o;
+
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        return eventName != null ? eventName.equals(that.eventName) : that.eventName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userName != null ? userName.hashCode() : 0;
+        result = 31 * result + (eventName != null ? eventName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "EventParticipantUpdateDTO{" +
+                "userName='" + userName + '\'' +
+                ", eventName='" + eventName + '\'' +
+                '}';
+    }
+
 }
