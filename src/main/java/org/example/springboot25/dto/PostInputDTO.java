cdc5d3782d2db00fef2826051a05e3050b02d314
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-15 00:19:13 +0200

    Add hashCode, toString and equals

--- src/main/java/org/example/springboot25/dto/PostInputDTO.java
+++ src/main/java/org/example/springboot25/dto/PostInputDTO.java
@@ -3,6 +3,8 @@ package org.example.springboot25.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.Objects;
+
 public class PostInputDTO {
 
     @NotBlank
@@ -37,4 +39,30 @@ public class PostInputDTO {
     public void setUserPostAuthorId(Long userPostAuthorId) {
         this.userPostAuthorId = userPostAuthorId;
     }
+
+    @Override
+    public String toString() {
+        return "PostInputDTO{" +
+                "postText='" + postText + '\'' +
+                ", postImageUrl='" + postImageUrl + '\'' +
+                ", userPostAuthorId=" + userPostAuthorId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostInputDTO that = (PostInputDTO) o;
+        return Objects.equals(postText, that.postText) &&
+                Objects.equals(postImageUrl, that.postImageUrl) &&
+                Objects.equals(userPostAuthorId, that.userPostAuthorId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postText, postImageUrl, userPostAuthorId);
+    }
+
+
 }
