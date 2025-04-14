33e4595ac72ebbe916c393e1be9e8c2d0322e0ee
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-14 23:45:19 +0200

    Add constructor, equals, hashCode, and toString

--- src/main/java/org/example/springboot25/dto/CatPhotoInputDTO.java
+++ src/main/java/org/example/springboot25/dto/CatPhotoInputDTO.java
@@ -3,6 +3,8 @@ package org.example.springboot25.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.Objects;
+
 public class CatPhotoInputDTO {
 
     @NotNull
@@ -13,6 +15,14 @@ public class CatPhotoInputDTO {
 
     @NotBlank
     private String catPhotoCaption;
+
+    public CatPhotoInputDTO() {}
+
+    public CatPhotoInputDTO(Long catPhotoCatId, String catPhotoUrl, String catPhotoCaption) {
+        this.catPhotoCatId = catPhotoCatId;
+        this.catPhotoUrl = catPhotoUrl;
+        this.catPhotoCaption = catPhotoCaption;
+    }
 
     public String getCatPhotoUrl() {
         return catPhotoUrl;
@@ -37,4 +47,29 @@ public class CatPhotoInputDTO {
     public void setCatPhotoCatId(Long catPhotoCatId) {
         this.catPhotoCatId = catPhotoCatId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CatPhotoInputDTO that = (CatPhotoInputDTO) o;
+        return Objects.equals(catPhotoCaption, that.catPhotoCaption) &&
+                Objects.equals(catPhotoUrl, that.catPhotoUrl) &&
+                Objects.equals(catPhotoCatId, that.catPhotoCatId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(catPhotoCaption, catPhotoUrl, catPhotoCatId);
+    }
+
+    @Override
+    public String toString() {
+        return "CatPhotoInputDTO{" +
+                "catPhotoName='" + catPhotoCaption + '\'' +
+                ", catPhotoUrl='" + catPhotoUrl + '\'' +
+                ", catPhotoCatId=" + catPhotoCatId +
+                '}';
+    }
+
 }
