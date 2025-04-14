ed37b7be8435efc2388a45025ff9745e6db8f0fa
Minhya <82528029+Minhya@users.noreply.github.com>
2025-04-14 15:27:45 +0200

    fixing-i18n (#27)
    * rebase from main
    
    * Fixed unnecessary titles in header.html
    
    * Fixed coderabbits nitpicks
    
    * Fixed merge conflicts

---
+++ src/main/java/org/example/springboot25/config/WebConfig.java
@@ -0,0 +1,45 @@
+package org.example.springboot25.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+
+    @Bean(name = "localeResolver")
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        return localeResolver;
+
+    }
+    @Bean
+public LocaleChangeInterceptor localeChangeInterceptor() {
+            LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+            interceptor.setParamName("lang");
+            return interceptor;
+        }
+
+        @Override
+public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(localeChangeInterceptor());
+        }
+
+
+}
