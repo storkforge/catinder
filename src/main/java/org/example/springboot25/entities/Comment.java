7edb0b82f8098ffdc10b23a1485f937437f82c9a
Eva Hottinen <ehottinen@gmail.com>
2025-04-16 21:29:49 +0200

    Created comment entity

---
+++ src/main/java/org/example/springboot25/entities/Comment.java
@@ -0,0 +1,41 @@
+package org.example.springboot25.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @NotBlank
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "userId")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private LocalDateTime createdAt;
+
+    // Getters & setters
+    public Long getCommentId() { return commentId; }
+    public void setCommentId(Long commentId) { this.commentId = commentId; }
+
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+    public User getAuthor() { return author; }
+    public void setAuthor(User author) { this.author = author; }
+
+    public Post getPost() { return post; }
+    public void setPost(Post post) { this.post = post; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
