12287562aecc1c950e097c4f4033721c5c175af7
Eva Hottinen <ehottinen@gmail.com>
2025-04-16 21:33:29 +0200

    Created comment dto input and output

---
+++ src/main/java/org/example/springboot25/dto/CommentOutputDTO.java
@@ -0,0 +1,62 @@
+package org.example.springboot25.dto;
+
+import java.time.LocalDateTime;
+
+public class CommentOutputDTO {
+
+    private Long commentId;
+    private String text;
+    private Long authorId;          // istället för bara username
+    private String authorUserName;
+    private Long postId;
+    private LocalDateTime createdAt;
+
+    // getters and setters
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorUserName() {
+        return authorUserName;
+    }
+
+    public void setAuthorUserName(String authorUserName) {
+        this.authorUserName = authorUserName;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
