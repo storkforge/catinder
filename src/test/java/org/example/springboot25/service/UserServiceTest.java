3ab8ae8e53b5681caebdbd13b852c60459df16e5
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-17 16:31:06 +0200

    Add getUserById test

---
+++ src/test/java/org/example/springboot25/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package org.example.springboot25.service;
+
+import org.example.springboot25.entities.User;
+import org.example.springboot25.mapper.UserMapper;
+import org.example.springboot25.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("Should return user when id exists")
+    void shouldUserWhenIdExists() {
+
+        Long userId = 1L;
+        User user = new User();
+        user.setUserId(user.getUserId());
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        User result = userService.findUserById(userId);
+        assertEquals(user, result);
+    }
+
+
+}
