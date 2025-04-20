bb60a82814b9b3862acc3485b70a1a88ff86cb3b
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-04-20 19:46:27 +0200

    Graph recommendation (#36)
    * Add GraphQL schema for recommendation
    
    * Add DTOs and mapper for recommendation
    
    * Add graphqlcontroller for recommendation
    
    * Add userId and catId to update input, rename file to .graphqls
    
    * Adjust line break for readability
    
    * Add validation to fields
    
    * Add null checks
    
    * Remove ! from deleteRecommendation
    
    * Add safe delete handling for recommendation

---
+++ src/main/java/org/example/springboot25/dto/RecommendationOutputDTO.java
@@ -0,0 +1,68 @@
+package org.example.springboot25.dto;
+
+public class RecommendationOutputDTO {
+
+    private Long id;
+    private String recommendationCategory;
+    private String recommendationProductName;
+    private String recommendationProductDescription;
+    private String recommendationProductLink;
+    private Long userId;
+    private Long catId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecommendationCategory() {
+        return recommendationCategory;
+    }
+
+    public void setRecommendationCategory(String recommendationCategory) {
+        this.recommendationCategory = recommendationCategory;
+    }
+
+    public String getRecommendationProductName() {
+        return recommendationProductName;
+    }
+
+    public void setRecommendationProductName(String recommendationProductName) {
+        this.recommendationProductName = recommendationProductName;
+    }
+
+    public String getRecommendationProductDescription() {
+        return recommendationProductDescription;
+    }
+
+    public void setRecommendationProductDescription(String recommendationProductDescription) {
+        this.recommendationProductDescription = recommendationProductDescription;
+    }
+
+    public String getRecommendationProductLink() {
+        return recommendationProductLink;
+    }
+
+    public void setRecommendationProductLink(String recommendationProductLink) {
+        this.recommendationProductLink = recommendationProductLink;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCatId() {
+        return catId;
+    }
+
+    public void setCatId(Long catId) {
+        this.catId = catId;
+    }
+}
