1edd982505cef81823a7620366f56711db941399
Malva Malmgren <71336957+malvamalmgren@users.noreply.github.com>
2025-04-20 15:31:12 +0200

    CSRF (#40)
    * Fix create event, secure CSRF, and add GetMapping for delete for consistency
    
    * Replace error-page.html with error.html
    
    * Fix header buttons
    
    * Add catPhoto slideshow for index.html
    
    * Add security context refresh on userRole update, and link to admin dashboard
    
    * Updated cat slideshow and admin dashboard
    
    * Add handle cats button with userRole check for index page
    
    * Fix after rebase
    
    * Cleanup
    
    * Fix AuthenticationToken in UserViewController PutMapping

--- src/main/java/org/example/springboot25/controller/HomeController.java
+++ src/main/java/org/example/springboot25/controller/HomeController.java
@@ -1,13 +1,29 @@
 package org.example.springboot25.controller;
 
+import org.example.springboot25.entities.Cat;
+import org.example.springboot25.entities.CatPhoto;
+import org.example.springboot25.service.CatPhotoService;
+import org.example.springboot25.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
+import java.util.List;
+
 @Controller
 public class HomeController {
+    private final CatPhotoService catPhotoService;
+
+    public HomeController(CatPhotoService catPhotoService) {
+        this.catPhotoService = catPhotoService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<CatPhoto> catPhotos = catPhotoService.getAllCatPhotos();
+        Collections.shuffle(catPhotos);
+        model.addAttribute("catPhotos", catPhotos);
         return "index";
     }
 }
