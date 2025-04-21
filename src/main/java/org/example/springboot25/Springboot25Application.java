d364ef1c17ae1d294be731ac77663a16b1028f39
LinusAltemark <Linalt0905@gmail.com>
2025-04-21 19:16:52 +0200

    Fixed Redis part2
    Signed-off-by: LinusAltemark <Linalt0905@gmail.com>

--- src/main/java/org/example/springboot25/Springboot25Application.java
+++ src/main/java/org/example/springboot25/Springboot25Application.java
@@ -3,15 +3,16 @@ package org.example.springboot25;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
 @EnableCaching
+@EnableJpaRepositories(basePackages = "org.example.springboot25.repository")
 public class Springboot25Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot25Application.class, args);
     }
-
 }
