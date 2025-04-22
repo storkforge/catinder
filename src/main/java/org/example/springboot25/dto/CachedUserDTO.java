998eb7d8a832b4572924b3e7cee95af4b71aeb30
LinusAltemark <Linalt0905@gmail.com>
2025-04-22 17:44:09 +0200

    Changed CachedUserDTO
    Signed-off-by: LinusAltemark <Linalt0905@gmail.com>

--- src/main/java/org/example/springboot25/dto/CachedUserDTO.java
+++ src/main/java/org/example/springboot25/dto/CachedUserDTO.java
@@ -1,16 +1,54 @@
 package org.example.springboot25.dto;
 
-import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
 import java.io.Serializable;
 
-@Data
+@RedisHash("CachedUser")
 public class CachedUserDTO implements Serializable {
-
-    private static final long serialVersionUID = 1L;
 
     private Long userId;
     private String userName;
     private String userEmail;
     private String userLocation;
     private String userRole;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(String userLocation) {
+        this.userLocation = userLocation;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
 }
