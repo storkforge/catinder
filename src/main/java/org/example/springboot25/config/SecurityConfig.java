dc67d205ecd684a6d2b4d3c1344b2b431d024b71
Malva Malmgren <malva.malmgren@gmail.com>
2025-04-22 14:00:11 +0200

    Fix after rebase

--- src/main/java/org/example/springboot25/config/SecurityConfig.java
+++ src/main/java/org/example/springboot25/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
