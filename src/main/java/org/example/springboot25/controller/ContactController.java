18b6a6fc080deb91a81936325da1dc0e4bd6fb52
Malva Malmgren <malva.malmgren@gmail.com>
2025-04-22 15:36:00 +0200

    Cleanup Contact

--- src/main/java/org/example/springboot25/controller/ContactController.java
+++ src/main/java/org/example/springboot25/controller/ContactController.java
@@ -1,20 +1,12 @@
 package org.example.springboot25.controller;
 
-import org.example.springboot25.service.ContactService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/contact")
 public class ContactController {
-    ContactService contactService;
-
-    public ContactController(ContactService contactService) {
-        this.contactService = contactService;
-    }
-
     @GetMapping
     public String contact() {
         return "contact";
